d5296c4ee5d0b008a8dd7c270ac45b224e803325
Priscilla Van Dyke <prisvd77@gmail.com>
2021-11-12 10:57:44 -0500

    customize app appearance

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connections
-app.config["MONGO_URI"] = "mongodb://localhost:27017//mars_app"
-mongo = PyMongo
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-app.route("/")
+@app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
     return render_template('index.html', mars=mars)
